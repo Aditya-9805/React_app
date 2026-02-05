60961a1594de56aa45981cbed8169a840f59bedd
Aditya <adityabhanwal0@gmail.com>
2026-02-05 17:39:22 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>This is my first web app deploy on aws</h1>
+      <h2>Iam learning Devops and this is my first staep towards it</h2>
+      <h3>by Aditya</h3>
+      <h4>Student of Chitkara University</h4>
+    </div>
+  )
+}
+
+export default App
